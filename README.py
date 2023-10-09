7cbc1412f388f2356ca7dc5c2c471a0d4d506585
faizan8787 <muhammadfaizansgc@gmail.com>
2023-10-09 16:21:20 +0500

    change

--- README.py
+++ README.py
@@ -1,1 +1,21 @@
-# Faizan_21_repo
+def calculator(x, y, sign):
+    if sign == '+':
+        return x + y
+    elif sign == '-':
+        return x - y
+    elif sign == '*':
+        return x * y
+    elif sign == '/':
+        if y != 0:
+            return x / y
+        else:
+            return "Division by zero is not allowed"
+    else:
+        return "Invalid operator"
+
+# Example usage:
+x=int(input('Enter a Number'))
+y=int(input('Enter second Number'))
+sign=input('Enter Sign')
+result = calculator(x, y, sign)
+print(f"{x} {sign} {y} = {result}")
